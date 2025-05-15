dc56d224bf0c292452597ac72c1c9036e10e45ac
Denis Pitsul <denia.pitsul@gmail.com>
2025-05-15 09:22:30 +0300

    remove useless if

--- src/App.jsx
+++ src/App.jsx
@@ -31,11 +31,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => {
-              if (selectedGood !== '') {
-                setSelectedGood('');
-              }
-            }}
+            onClick={() => setSelectedGood('')}
           />
         )}
       </h1>
